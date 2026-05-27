0a0cf13e74ce2ca22667a3029309c8535ce8cecc
Saizade <sai.zade01@gmail.com>
2026-05-27 15:34:36 +0530

    Fix Sequelize import

--- server/config/database.js
+++ server/config/database.js
@@ -1,16 +1,22 @@
+const { Sequelize } = require("sequelize");
+
 const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD, {
         host: process.env.DB_HOST,
         port: process.env.DB_PORT || 3306,
-        dialect: 'mysql',
+        dialect: "mysql",
         logging: false,
-        dialectOptions: process.env.NODE_ENV === 'production' ? {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false,
-            },
-        } : {},
+        dialectOptions: process.env.NODE_ENV === "production" ?
+            {
+                ssl: {
+                    require: true,
+                    rejectUnauthorized: false,
+                },
+            } :
+            {},
     }
 );
+
+module.exports = sequelize;
